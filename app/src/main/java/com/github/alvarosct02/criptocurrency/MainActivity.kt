afb668b9954b098c455e3666a11f819f642b1392
Alvaro Santa Cruz <alvarosantacruz@Alvaros-Mac-mini.local>
2021-01-14 07:25:12 -0500

    [S1] New Project

---
+++ app/src/main/java/com/github/alvarosct02/criptocurrency/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.alvarosct02.criptocurrency
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
